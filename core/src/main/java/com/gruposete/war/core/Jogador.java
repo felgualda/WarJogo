fe2da7db22af8d5b2d982f209fd47dc48b0475a6
Felipe Gualda <felipegualda@id.uff.br>
2025-10-31 22:06:33 -0300

    #18: Criar a estrutura de um jogador

---
+++ core/src/main/java/com/gruposete/war/core/Jogador.java
@@ -0,0 +1,77 @@
+package com.gruposete.war.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jogador {
+    private String nome;
+    private CorJogador cor;
+
+    private List<Territorio> territorios;
+    private List<Carta> cartas;
+    private Objetivo objetivo;
+
+    private int exercitosDisponiveis;
+
+    public Jogador(String nome, CorJogador cor) {
+        this.nome = nome;
+        this.cor = cor;
+        this.territorios = new ArrayList<>();
+        this.cartas = new ArrayList<>();
+        this.exercitosDisponiveis = 0;
+    }
+
+    // GETTERS E SETTERS
+
+    public String getNome() {
+        return nome;
+    }
+
+    public CorJogador getCor() {
+        return cor;
+    }
+
+    public List<Territorio> getTerritorios() {
+        return territorios;
+    }
+
+    public List<Carta> getCartas() {
+        return cartas;
+    }
+
+    public Carta getCartaByIndex(int index) {
+        return cartas.get(index);
+    }
+
+    public int getExercitosDisponiveis() {
+        return exercitosDisponiveis;
+    }
+
+    public Objetivo getObjetivo() {
+        return objetivo;
+    }
+
+    public void setObjetivo(Objetivo objetivo) {
+        this.objetivo = objetivo;
+    }
+
+    // OUTROS
+
+    public int adicionarTerritorio(Territorio t) {
+        territorios.add(t);
+        return territorios.size();
+    }
+
+    public int removerTerritorio(Territorio t) {
+        territorios.remove(t);
+        return territorios.size();
+    }
+
+    public void adicionarCarta(Carta c) {
+        cartas.add(c);
+    }
+
+    public void removerCarta(Carta c) {
+        cartas.remove(c);
+    }
+}
